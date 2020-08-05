cbaa2f76f92c211d44beb73609c8a2375abf10b0
djones36 <daniel.jones2000@gmail.com>
2020-08-05 16:18:23 -0700

    javascript string reminder

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Test of scripting file");
+console.log("Test of external Javascript file");
+
+var num1 = 10;
+var num2 = 20;
+
+console.log(num1 + num2);
+
+var value = "Dogman"; // string saved
+
+console.log(value);
+
+var name = "John";
+
+console.log("+ to combine two string vars " + name + " " + value);
+console.log(`Practicing string literals with backticks ${name} ${value}`);
